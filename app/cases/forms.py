eba9d7fc1aaa894fa9b1903bba0b9eb91c70a14e
David Vallee Delisle <dvd@redhat.com>
2018-02-17 22:21:43 -0500

    Little refactor of yank and toxification

--- app/cases/forms.py
+++ app/cases/forms.py
@@ -6,5 +6,9 @@ class CaseSearchForm(FlaskForm):
     """
     Form to lookup a case and get the sosreports
     """
-    casenum = StringField('casenum', validators=[DataRequired(), Regexp('^0[0-9]{7}$',  message="Invalid case number")], id='casenum')
+    casenum = StringField('casenum', 
+                          validators=[DataRequired(),
+                                      Regexp('^0[0-9]{7}$',
+                                      message="Invalid case number")],
+                                      id='casenum') 
     submit = SubmitField('Search')
--- app/cases/views.py
+++ app/cases/views.py
@@ -1,5 +1,6 @@
 # Flask
-from flask import flash, redirect, render_template, url_for, current_app, request, Response, abort, jsonify
+from flask import flash, redirect, render_template, url_for
+from flask import current_app, request, abort, jsonify
 from flask_login import login_required
 
 # Global imports
@@ -22,99 +23,129 @@ from app.reports import add_report
 from app.helpers import sysstat
 
 
+@cases.route('/<case>/yank?force=<force>')
+@cases.route('/<case>/yank')
+@login_required
+def yank(case, force=None):
+    """
+    Yank files to collab-shell
+    :param case: case id
+    :param force: if true, we force the yank
+    :return: redirect to /cases/caseid
+    """
+    if force == 'True':
+        command = "/bin/bash /usr/bin/yank --force " + case
+    else:
+        command = "/bin/bash /usr/bin/yank " + case
+    try:
+        result = subprocess.check_output([command], shell=True)
+    except subprocess.CalledProcessError as e:
+        flash(u"Unable to yank case %s: %s" % (case,e.output),
+              category="error")
+    return redirect(url_for("cases.search", case=case), 303)
+
 
 @cases.route('/', methods=['GET', 'POST'])
-@cases.route('/?case=<case>&yank=<yank>', methods=['GET', 'POST'])
-@cases.route('/?case=<case>&yank=<yank>&force=<force>', methods=['GET', 'POST'])
+@cases.route('/<case>', methods=['GET'])
 @login_required
-def search(case=None, yank=None, force=None):
+def search(case=None):
     """
     Handle requests for cases
+    :param case: case id
+    :return: search.html
     """
-    def yank(case, force=None):
-        if force == 'True':
-            command = "/bin/bash /usr/bin/yank --force " + case
-        elif force is None:
-            command = "/bin/bash /usr/bin/yank " + case
-
-        logging.debug("searching for attachment for case %s", case)
-        logging.debug("executing command: %s", command)
-        try:
-            result = subprocess.check_output([command], shell=True)
-        except subprocess.CalledProcessError as e:
-            return "An error occurred while executing command."
 
     form = CaseSearchForm()
     if case is None and form.validate_on_submit():
         case = form.casenum.data
-    elif yank is not None and case is not None:
-        yank(case, force)
     if case is not None:
         casepath = "/cases/" + case
         reportList = []
         jsonregex = []
-        combinedregex = re.compile("(" + ")|(".join(current_app.config["REPORT_FILE_NAMES"]) + ")")
+        # Building regex for citellus[0-9]+.json and magui[0-9]+.json
+        combinedregex = re.compile("(" + ")|(".join(
+                                       current_app.config["REPORT_FILE_NAMES"])
+                                    + ")")
+        # Looping through the files
         for root, dirs, files in os.walk(casepath, topdown=True):
             matched = filter(combinedregex.match, files)
-            if len(matched) > 0:
-                for f in matched:
-                    fullname = root + "/" + f
-                    sardir = root + "/var/log/sa"
-                    if os.path.isdir(sardir):
-                        sarfiles = sysstat.sysstat.get_file_date(sardir)
-                    else:
-                        sarfiles = []
-                    report, results, report_changed = add_report(
-                        fullname, form.casenum.data)
-                    counts = loop_checks(report.id, results, report.source,
-                                         report_changed)
-                    # add report to the web interface
-                    if report.source == "magui":
-                        icon = "microchip"
-                    else:
-                        icon = "cog"
-                    reportList.append({"fullname": fullname,
-                                "name": "/".join(root.split("/")[-2:]), 
-                                "report_id": report.id,
-                                "icon": icon, 
-                                "sarfiles": sarfiles,
-                                "checks_total": counts["total"], 
-                                "checks_fail": counts[20], 
-                                "checks_skip": counts[30], 
-                                "checks_okay": counts[10], 
-                                "analyze_duration": report.analyze_duration,
-                                "size": report.size,
-                                "hr_size": report.get_hr_size(2),
-                                "collect_time": report.collect_time,
-                                "machine_id": report.machine_id,
-                                "source": report.source})
-
-            if root.count(os.sep) - casepath.count(os.sep) == 2:
-                del dirs[:]
-
-        return render_template('cases/search.html', form=form, casenum = case, reportList=reportList, title='Search sosreport in case')
+            if len(matched) == 0:
+                continue
+            for f in matched:
+                fullname = root + "/" + f
+                sardir = root + "/var/log/sa"
+                if os.path.isdir(sardir):
+                    sarfiles = sysstat.sysstat.get_file_date(sardir)
+                else:
+                    sarfiles = []
+                report, results, report_changed = add_report(fullname, 
+                                                             form.casenum.data)
+                counts = loop_checks(report.id, results, report.source,
+                                     report_changed)
+                # add report to the web interface
+                if report.source == "magui":
+                    icon = "microchip"
+                else:
+                    icon = "cog"
+                reportList.append({"fullname": fullname,
+                            "name": "/".join(root.split("/")[-2:]), 
+                            "report_id": report.id,
+                            "icon": icon, 
+                            "sarfiles": sarfiles,
+                            "checks_total": counts["total"], 
+                            "checks_fail": counts[20], 
+                            "checks_skip": counts[30], 
+                            "checks_okay": counts[10], 
+                            "analyze_duration": report.analyze_duration,
+                            "size": report.size,
+                            "hr_size": report.get_hr_size(2),
+                            "collect_time": report.collect_time,
+                            "machine_id": report.machine_id,
+                            "source": report.source})
+    
+        # We don't go deeper than 2 directories
+        # to save some time
+        if root.count(os.sep) - casepath.count(os.sep) == 2:
+            del dirs[:]
+    
+        return render_template('cases/search.html', form=form, casenum = case, 
+                           reportList = reportList, 
+                           title = 'Search sosreport in case')
+    # There was an error with the form submission
     elif request.method == "POST":
         flash_errors(form)
-    return render_template('cases/search.html', form=form, title='Search sosreport in case')
+    # If no caseid, we return standard form
+    return render_template('cases/search.html')
 
 @cases.route('/compare', methods=['POST','GET'])
 @login_required
 def compare():
+    """
+    Compare multiple reports
+    :param json: array of report ids
+    :return: json object: status, msg
+    """
     reports = request.get_json()
     if len(reports) < 2:
-        logging.debug("We need more reports to compare. Got %s reports", len(reports))
-        abort(400)
+        return jsonify({ "status": "danger",
+                        "msg": "We need at least 2 reports to compare"})
     rlist = []
     for r in reports:
         report = db.session.query(Report).filter_by(id=r).first()
         if report is None:
-            logging.debug("Report not found. ID: %s", r)
-            abort(404)
+            return jsonify({ "status": "danger", "msg": "Report not found" })
         rlist.append(os.path.dirname(report.path))
-    outfile = re.match("(/cases/[0-9]+/)", rlist[0]).group(1) + "magui" + str(datetime.datetime.today().strftime('%Y%m%d%H%M%S')) + ".json"
+    outfile = re.match("(/cases/[0-9]+/)", rlist[0]).group(1) + "magui" \
+              + str(datetime.datetime.today().strftime('%Y%m%d%H%M%S')) + ".json"
     return magui(outfile, rlist)
 
 def magui(outfile, reports):
+    """
+    Runs magui (compare tool) on reports
+    :param outfile: json output file generated by magui
+    :param reports: list of report ids
+    :return: json object with execution status
+    """
     args = ["python", current_app.config["CITELLUS_PATH"] + "/magui.py"]
     args.extend(["--loglevel", "DEBUG", "-o", outfile])
     args.extend(reports)
@@ -122,10 +153,7 @@ def magui(outfile, reports):
     try:
         result = subprocess.check_output([command], shell=True)
     except subprocess.CalledProcessError as e:
-        return jsonify({ "status": "danger", "msg": "An error occurred while executing command."})
-    return jsonify({ "status": "success", "msg": "Succesfully generated compare report"})
-
-def has_no_empty_params(rule):
-    defaults = rule.defaults if rule.defaults is not None else ()
-    arguments = rule.arguments if rule.arguments is not None else ()
-    return len(defaults) >= len(arguments)
+        return jsonify({ "status": "danger", 
+                        "msg": "An error occurred while executing command."})
+    return jsonify({ "status": "success", 
+                    "msg": "Succesfully generated compare report"})
